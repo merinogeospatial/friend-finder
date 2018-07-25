1c578860c1883c820ea42e5b31d6c97d3ad0ba2b
R <merinogeospatial@gmail.com>
2018-07-25 17:13:38 -0400

    checkpoint - routes and api route working

--- server.js
+++ server.js
@@ -1,2 +1,71 @@
-const express = require('express');
-const path = require('path');
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+var friends = require('./app/data/friends.js');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing and point static to public
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+// app.use("/public", express.static(path.join(__dirname, 'public')));
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/home.html"));
+});
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
+
+// API
+app.get("/api/friends", function(req, res) {
+  return res.json(friends);
+});
+
+// Displays a single character, or returns false
+// app.get("/api/friends", function(req, res) {
+//   friends = req.params.character;
+
+//   console.log(chosen);
+
+//   for (var i = 0; i < characters.length; i++) {
+//     if (chosen === characters[i].routeName) {
+//       return res.json(characters[i]);
+//     }
+//   }
+
+//   return res.json(false);
+// });
+
+// Create New Characters - takes in JSON input
+app.post("/api/characters", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body-parser middleware
+  var newcharacter = req.body;
+
+  // Using a RegEx Pattern to remove spaces from newCharacter
+  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+
+  console.log(newcharacter);
+
+  characters.push(newcharacter);
+
+  res.json(newcharacter);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
