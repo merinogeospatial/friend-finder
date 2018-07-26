f2b0905e2a561c1a45945338fff20ef666d9fd4c
R <merinogeospatial@gmail.com>
2018-07-26 18:57:26 -0400

    update data dir

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,15 +3,15 @@ let friends =
         "name":"Ahmed",
         "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
         "scores":[
-            5,
-            1,
-            4,
-            4,
-            5,
             1,
             2,
+            3,
+            4,
+            5,
             5,
             4,
+            3,
+            2,
             1
         ]
     },
@@ -28,6 +28,54 @@ let friends =
             2,
             5,
             4,
+            1
+        ]
+    },
+    {
+        "name":"Richard",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name":"Nic",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name":"Ricky Bobby",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
             1
         ]
     }]
